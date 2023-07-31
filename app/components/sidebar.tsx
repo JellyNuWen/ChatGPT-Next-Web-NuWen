45e222eba6968c88dd19563ab10ee65d88572143
Cole <46254944+Cole404@users.noreply.github.com>
2023-07-31 16:25:59 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          打造自己的人工智能助手.
+          1133自己的人工智能助手.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
