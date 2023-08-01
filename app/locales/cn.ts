80974ae0bbf945bed675088712e5359b043407a5
Cole <46254944+Cole404@users.noreply.github.com>
2023-08-01 18:05:15 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -257,7 +257,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "这是一个基于[ChatGPT-Next-Web](https://github.com/Yidadaa/ChatGPT-Next-Web)打造的个人AI助手，仅供个人使用，分享仅做展示。**提问：** 这个定制化主题很好看，我也能使用吗？**回答：** 当然可以，这是我修改后的[GitHub开放仓库](https://github.com/Cole404/ChatGPT-Next-Web)，欢迎复制、参考～欢迎来看看[个人网站](kamiyd.com)",
+    BotHello: "这是一个基于[ChatGPT-Next-Web](https://github.com/Yidadaa/ChatGPT-Next-Web)打造的个人AI助手，仅供个人使用，分享仅做展示。<br>**提问：** 这个定制化主题很好看，我也能使用吗？<br>**回答：** 当然可以，这是我修改后的[GitHub开放仓库](https://github.com/Cole404/ChatGPT-Next-Web)，欢迎复制、参考～<br>欢迎来看看[个人网站](https://kamiyd.com)",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
