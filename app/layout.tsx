85b573376f0be8e505fba2c849214bba48873cef
怒文 <153164365+JellyNuWen@users.noreply.github.com>
2023-12-07 20:17:28 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Kami’s ChatGPT",
-  description: "A personal AI assistant built on ChatGPT-Next-Web.",
+  title: "NuWen’s ChatGPT",
+  description: "专属你的个人AI.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "Kami’s ChatGPT",
+    title: "NuWen’s ChatGPT",
     statusBarStyle: "default",
   },
 };
